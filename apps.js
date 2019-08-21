2271dde4d3a1e52c9aa5ac6e433c1d115c871531
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-21 20:15:57 +0700

    slot in action

--- apps.js
+++ apps.js
@@ -4,8 +4,13 @@
 // })
 
 // <!-- =========== Power Components =========== -->
+// Vue.component('textbold',{
+//   template: '<b>This text come from component</b>'
+// })
+
+// <!-- =========== Slot in action =========== -->
 Vue.component('textbold',{
-  template: '<b>This text come from component</b>'
+  template: '<b><slot></slot></b>'
 })
 
 var app = new Vue({
