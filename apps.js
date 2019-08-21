db985c0e1b858c470a4c5e20ded59a9ac46202d8
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-21 20:36:51 +0700

    component, slot, and array

--- apps.js
+++ apps.js
@@ -14,8 +14,26 @@
 // })
 
 // <!-- =========== Multi element =========== -->
-Vue.component('multitext',{
-  template: '<div><b>text bold</b><br><i>text italic</i><br><u>text underline</u></div>'
+// Vue.component('multitext',{
+//   template: '<div><b>text bold</b><br><i>text italic</i><br><u>text underline</u></div>'
+// })
+
+// <!-- =========== Component, Slot, and Array =========== -->
+Vue.component('list',{
+  template: '<div><people v-for="p in peoplelist"> {{p.name}} age {{p.age}}</people></div>',
+  data: function(){
+    return {
+      peoplelist: [
+        { name: 'andre', age: '30'},
+        { name: 'sule', age: '40'},
+        { name: 'tukul', age: '50'},
+      ]
+    }
+  }
+})
+
+Vue.component('people',{
+  template: '<li><slot></slot></li>'
 })
 
 var app = new Vue({
